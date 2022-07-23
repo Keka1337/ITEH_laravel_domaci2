a547df91ef9a543c17edd9bc7fe5cafdd0318556
Keka1337 <75698448+Keka1337@users.noreply.github.com>
2022-07-23 09:19:24 +0200

    Kreiran model Paintning i izvrsena migracija

---
+++ loving-art/database/migrations/2022_07_23_071618_create_paintings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaintingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paintings', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('artist_id');
+            // $table->foreignId('art_period_id');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paintings');
+    }
+};
